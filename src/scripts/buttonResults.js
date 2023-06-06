749f059f82dbef64d53dca8c527b190eb750d045
Denis Sharov <denissharov197@gmail.com>
2023-06-07 00:50:29 +0300

    [FEATURE] MLTL-14838843841 Example of working with DOM Api

--- src/scripts/buttonResults.js
+++ src/scripts/buttonResults.js
@@ -8,7 +8,7 @@ const concat = () => {
     const secondSelectResult = document.getElementById("secondSelect"); //достаем значение из второго селекта
     let div = document.createElement("div");
     div.setAttribute("id", "myDiv")
-    let e = document.getElementById("myDiv");
+    const e = document.getElementById("myDiv");
     if (e !== null) {
         e.remove()
     }
