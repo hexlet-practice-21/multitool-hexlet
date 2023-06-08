65ee8ff378205b94a9d5435f5ad09b5361f839e4
AlenaSmile <alenaeliseenko2003@gmail.com>
2023-06-08 19:02:55 +0300

    [FEATURE] MLTL-6 function convert speed

--- __tests__/stub.test.js
+++ __tests__/stub.test.js
@@ -2,24 +2,39 @@
 
 import { expect, test } from '@jest/globals';
 import returnTrue from '../src/scripts/empty.js';
-import speedConversion from '../src/scripts/index.js';
+import speedConversion from '../src/scripts/convertspeed.js';
 
 test('stub', () => {
   expect(returnTrue()).toBe(true);
 });
 
-test('speedConversion', () => {
-  // @ts-ignore
-  expect(speedConversion(10, 'kilph in mph')).toEqual(6.21371);
-  // @ts-ignore
-  expect(speedConversion(10, 'kilph in knph')).toEqual(5.39957);
-  // @ts-ignore
-  expect(speedConversion(10, 'kmph in kilph')).toEqual(16.09344);
-  // @ts-ignore
-  expect(speedConversion(10, 'mph in knph')).toEqual(8.68977);
-  // @ts-ignore
-  expect(speedConversion(10, 'knph in mph')).toEqual(11.50779);
-  // @ts-ignore
-  expect(speedConversion(10, 'knph in kilph')).toEqual(18.52);
-  expect(speedConversion()).toBeNull();
+test('should convert kilometers to all units', () => {
+  const value = 10;
+  const speed = 'km';
+  const result = speedConversion(value, speed);
+  expect(result).toEqual({
+    kilometers: 10,
+    miles: 6.21371,
+    knots: 5.39957,
+  });
+});
+test('should convert miles to all units', () => {
+  const value = 15;
+  const speed = 'mil';
+  const result = speedConversion(value, speed);
+  expect(result).toEqual({
+    miles: 15,
+    kilometers: 24.14016,
+    knots: 13.03465,
+  });
+});
+test('should convert knots to all units', () => {
+  const value = 20;
+  const speed = 'knots';
+  const result = speedConversion(value, speed);
+  expect(result).toEqual({
+    knots: 20,
+    miles: 23.01558,
+    kilometers: 37.04,
+  });
 });
---
+++ src/scripts/convertspeed.js
@@ -0,0 +1,29 @@
+const speedConversion = (value, speed) => {
+  const speeds = {
+    km: {
+      kilometers: (values) => values,
+      miles: (values) => (+(values / 1.609344).toFixed(5)),
+      knots: (values) => (+(values / 1.852).toFixed(5)),
+    },
+    mil: {
+      miles: (values) => values,
+      kilometers: (values) => (+(values * 1.609344).toFixed(5)),
+      knots: (values) => (+(values / 1.150779).toFixed(5)),
+    },
+    knots: {
+      knots: (values) => values,
+      miles: (values) => (+(values * 1.150779).toFixed(5)),
+      kilometers: (values) => (+(values * 1.852).toFixed(5)),
+    },
+  };
+  if (speed in speeds) {
+    const results = {};
+    Object.keys(speeds[speed]).forEach((key) => {
+      results[key] = speeds[speed][key](value);
+    });
+    return results;
+  }
+  return null;
+};
+
+export default speedConversion;
